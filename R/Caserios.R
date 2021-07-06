3c4b7ba7cf0741acdf752adf279ea544d73e8964
Hondunauta <klauswiese@users.noreply.github.com>
2021-07-06 13:17:03 -0600

    Add files via upload
    Datos paquete

---
+++ R/Caserios.R
@@ -0,0 +1,16 @@
+#' PNLT spatial data
+#'
+#' This package contains spatial datasets of La Tigra National Park
+#' @docType package
+#' @name pnlt
+#' @aliases pnlt pnlt-package
+NULL
+
+#' "Caserios en La Tigra National Park"
+#'
+#' A data set containing  the forest classification for
+#' the year 2014.
+#'
+#' @source Fundación Amigos de la Tigra
+#' @format Simple feature collection
+"Caserios"
---
+++ R/Municipios.R
@@ -0,0 +1,16 @@
+#' PNLT spatial data
+#'
+#' This package contains spatial datasets of La Tigra National Park
+#' @docType package
+#' @name pnlt
+#' @aliases pnlt pnlt-package
+NULL
+
+#' "Municipalities in La Tigra National Park"
+#'
+#' A data set containing  the Municipalities in
+#' La Tigra National Park, Honduras
+#'
+#' @source Fundación Amigos de la Tigra
+#' @format Simple feature collection with 4 polygons
+"Municipios"
