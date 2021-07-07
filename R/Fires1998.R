6bfb623910e57083af06e37f39d5ccf9fede7cd8
Hondunauta <klauswiese@users.noreply.github.com>
2021-07-07 11:03:36 -0600

    Add files via upload

---
+++ R/Fires1998.R
@@ -0,0 +1,16 @@
+#' PNLT spatial data
+#'
+#' This package contains spatial datasets of La Tigra National Park
+#' @docType package
+#' @name pnlt
+#' @aliases pnlt pnlt-package
+NULL
+
+#' "Fires from year 1998 in La Tigra National Park"
+#'
+#' A data set containing the fires from year 1998 in
+#' La Tigra National Park, Honduras
+#'
+#' @source Digitized by K. Wiese from landsat images
+#' @format Simple feature collection with polygons
+"Fires1998"
---
+++ R/Fires2020.R
@@ -0,0 +1,16 @@
+#' PNLT spatial data
+#'
+#' This package contains spatial datasets of La Tigra National Park
+#' @docType package
+#' @name pnlt
+#' @aliases pnlt pnlt-package
+NULL
+
+#' "Fires from year 2020 in La Tigra National Park"
+#'
+#' A data set containing the fires from year 2011 in
+#' La Tigra National Park, Honduras
+#'
+#' @source Digitized by K. Wiese from sentinel-2 images
+#' @format Simple feature collection with polygons
+"Fires2020"
